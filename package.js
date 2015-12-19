8533064b202a9107ff9ceef04dc78e7f311eaef7
Dyaa Eldin <mail@dyaa.me>
2015-12-19 22:48:05 +0200

    publishing documentation to atmospherejs

--- package.js
+++ package.js
@@ -2,7 +2,8 @@ Package.describe({
     name: 'dyaa:authenticator',
     summary: 'Google authenticator methods for quick integration',
     version: '0.1.0',
-    git: 'https://github.com/dyaa/Google-Authenticator'
+    git: 'https://github.com/dyaa/Meteor-Google-Authenticator',
+    documentation: 'Readme.md'
 });
 
 Package.onUse(function(api) {
